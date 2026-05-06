5f041d51dc3afe46637ef2184e42b8ac6f0f3bc8
faceless-truth <214740682+faceless-truth@users.noreply.github.com>
2026-05-06 07:03:24 -0400

    Initial commit

---
+++ client/src/data/blogPosts.ts
@@ -0,0 +1,17 @@
+export interface BlogPost {
+  slug: string;
+  title: string;
+  metaTitle: string;
+  metaDescription: string;
+  keywords: string;
+  excerpt: string;
+  author: string;
+  publishDate: string;
+  readTime: string;
+  category: string;
+  heroEmoji: string;
+  content: string;
+}
+
+export const blogPosts: BlogPost[] = [
+];
